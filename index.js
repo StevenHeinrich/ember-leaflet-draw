20a45d4ceeb417f30f3087d5cd383d3607b29244
Steven Heinrich <steven.heinrich@gmail.com>
2017-01-29 11:58:44 -0500

    add included bower imports

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
 'use strict';
 
+const path = require('path');
+
 module.exports = {
-  name: 'ember-leaflet-draw'
+  name: 'ember-leaflet-draw',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'leaflet-draw.js'));
+    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'leaflet-draw.css'));
+  }
 };
