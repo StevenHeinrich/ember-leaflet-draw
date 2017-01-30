5d2e97df6b7467c2d95693103aeda5430e985ada
Steven Heinrich <steven.heinrich@gmail.com>
2017-01-29 22:09:45 -0500

    add included bower imports for leaflet-draw image assets

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'leaflet-draw.js'));
-    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'leaflet-draw.css'));
+    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'leaflet.draw.js'));
+    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'leaflet.draw.css'));
+
+    var imagesDestDir = '/assets/images';
+    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'images/spritesheet-2x.png'), { destDir: imagesDestDir });
+    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'images/spritesheet.png'), { destDir: imagesDestDir });
+    app.import(path.join(app.bowerDirectory, 'leaflet-draw', 'dist', 'images/spritesheet.svg'), { destDir: imagesDestDir });
   }
 };
