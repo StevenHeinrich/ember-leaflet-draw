6d7318a1150142d6a8bd73244a5dcc69ad094dea
Steven Heinrich <steven.heinrich@gmail.com>
2017-01-30 18:09:13 -0500

    add very basic component for adding the Drawing Control to the map

---
+++ addon/components/draw-control.js
@@ -0,0 +1,40 @@
+// TODO: determine what is really needed from BaseLayer and create a new starting point, BaseControl (right now, this is just piggybacking off BaseLayer)
+import BaseLayer from 'ember-leaflet/components/base-layer';
+
+export default BaseLayer.extend({
+  leafletEvents: [
+    'draw:edited',
+    'draw:editmove',
+    'draw:editstart',
+    'draw:editstop',
+    'draw:editvertex'
+  ],
+
+  leafletOptions: [
+    'edit',
+    'draw',
+    'position'
+  ],
+
+  createLayer() {}, // Must override createLayer if extending BaseLayer
+
+  didInsertParent() {
+    this._layer = this.createLayer();
+    this._addObservers();
+    this._addEventListeners();
+
+    let map = this.get('parentComponent._layer');
+
+    if(map) {
+      console.log('map', map);
+      let options = this.getProperties('position', 'draw', 'edit');
+      if(!options.position) {
+        options.position = 'topleft';
+      }
+      if(!options.draw) {
+        options.draw = L.drawLocal.draw;
+      }
+      map.addControl(new this.L.Control.Draw(options));
+    }
+  }
+});
