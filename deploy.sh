df7999ba754b6e4d338cc289b61ed48693c8d357
Steven Heinrich <steven.heinrich@gmail.com>
2017-02-03 18:18:05 -0500

    update deploy script for travis

--- deploy.sh
+++ deploy.sh
@@ -30,12 +30,6 @@ cd dist
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled dist (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-  echo "No changes to the output on this push; exiting."
-  exit 0
-fi
-
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
