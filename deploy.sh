1f571d4f86dae0df26158a494a2759ad7afc9808
Steven Heinrich <steven.heinrich@gmail.com>
2017-02-03 19:17:17 -0500

    update deploy script for travis

--- deploy.sh
+++ deploy.sh
@@ -12,21 +12,24 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into dist/
+# Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO dist
-cd dist
+git clone $REPO out
+cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clear existing contents
-rm -rf dist/**/* || exit 0
+rm -rf out/**/* || exit 0
 
 # Build the Ember app
 ember build
 
+# Copy dist files into out
+cp -a dist/. out/
+
 # Now let's go have some fun with the cloned repo
-cd dist
+cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
