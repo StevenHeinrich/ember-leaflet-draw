32201ea3e208addd3c4abcb71e2b80c62ad81ce0
Steven Heinrich <steven.heinrich@gmail.com>
2017-02-12 09:25:28 -0500

    update map marker icon images in dummy app

--- tests/dummy/app/controllers/home.js
+++ tests/dummy/app/controllers/home.js
@@ -13,8 +13,8 @@ export default Ember.Controller.extend({
     },
     marker: {
       icon: new L.Icon({
-        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconUrl: 'assets/images/map-markers/marker-icon-2x-green.png',
+        shadowUrl: 'assets/images/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
