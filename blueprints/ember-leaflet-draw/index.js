2bb09573f8e84c86d483ca7226f26d591c6e1e28
Benjamin Lodi <brlodi@gmail.com>
2017-06-15 14:38:31 -0500

    Pull leaflet-draw from NPM instead of Bower
    Directly mirrors the change made in ember-leaflet to move to NPM leaflet

--- blueprints/ember-leaflet-draw/index.js
+++ blueprints/ember-leaflet-draw/index.js
@@ -1,10 +1,10 @@
-/*jshint node:true*/
-module.exports = {
-  description: 'add leaflet-draw assets, using bower',
-
-  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
-
-  afterInstall: function() {
-    return this.addBowerPackageToProject('leaflet-draw', '~0.4.9');
-  }
-};
+// /*jshint node:true*/
+// module.exports = {
+//   description: 'add leaflet-draw assets, using bower',
+//
+//   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
+//
+//   afterInstall: function() {
+//     return this.addBowerPackageToProject('leaflet-draw', '~0.4.9');
+//   }
+// };
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 /* jshint node: true */
 'use strict';
+const resolve = require('resolve');
+const path = require('path');
+const Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-leaflet-draw',
 
+  treeForVendor: function() {
+    let dist = path.join(this.pathBase('leaflet-draw'), 'dist');
+    return new Funnel(dist, { destDir: 'leaflet-draw' });
+  },
+
   included: function(app) {
     this._super.included.apply(this, arguments);
-    
+
     // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
     // use that.
     if (typeof this._findHost === 'function') {
@@ -21,8 +29,8 @@ module.exports = {
     do {
      app = current.app || app;
     } while (current.parent.parent && (current = current.parent));
-    
-    var baseDir = app.bowerDirectory + '/leaflet-draw/dist/';
+
+    var baseDir = 'vendor/leaflet-draw/';
 
     app.import(baseDir + 'leaflet.draw.js');
     app.import(baseDir + 'leaflet.draw.css');
@@ -31,5 +39,9 @@ module.exports = {
     app.import(baseDir + 'images/spritesheet-2x.png', { destDir: imagesDestDir });
     app.import(baseDir + 'images/spritesheet.png', { destDir: imagesDestDir });
     app.import(baseDir + 'images/spritesheet.svg', { destDir: imagesDestDir });
+  },
+
+  pathBase: function(packageName) {
+    return path.dirname(resolve.sync(packageName + '/package.json', { basedir: __dirname }));
   }
 };
